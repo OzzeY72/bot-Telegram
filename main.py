94f03e33ce23ef92d1c3035ffa04c8bf7f282a6f
OzzeY72 <nabacap72@gmail.com>
2023-11-09 20:37:02 +0100

    increase log level

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
+        level=logging.DEBUG
     )
     _handlers = {}
 
